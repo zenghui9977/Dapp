1edcfcf2f0c0488d5f1822236e0f8ad36abec0d6
zenghui9977 <35621224+zenghui9977@users.noreply.github.com>
2018-12-21 20:13:51 -0800

    the configure of antd
    configure

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         }
       },
       {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
         test: /\.sol/,
         use: [
           {
